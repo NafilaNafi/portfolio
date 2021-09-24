05f707239b62579c38f16f3d70ba68aca64185ee
Nafila <nafila@outlook.com>
2021-09-24 12:55:37 +0530

    Update portfolios.js

--- src/data/portfolios.js
+++ src/data/portfolios.js
@@ -7,7 +7,6 @@ import img2 from '../img/portImages/movielist.jpg';
 import img3 from '../img/portImages/maya-3.jpg';
 import img4 from '../img/portImages/traveller.png';
 import img5 from "../img/portImages/verify phone.jpg";
-import img6 from "../img/portImages/portfolio.png";
 
 
 const portfolios = [
@@ -40,15 +39,6 @@ const portfolios = [
   },
   {
     id: 4,
-    category: "Javascript",
-    image: img6,
-    link1: "https://github.com/NafilaNafi/portfolio",
-    link2: "https://nafi-codes.netlify.app/",
-    title: "Fullstack Portfolio",
-    text: "Personal Portfolio.",
-  },
-  {
-    id: 5,
     category: "React JS",
     image: img1,
     link1: "https://github.com/NafilaNafi/Adventure",
@@ -57,7 +47,7 @@ const portfolios = [
     text: "Find your amazing trip destinations using Adventure App",
   },
   {
-    id: 6,
+    id: 5,
     category: "Javascript",
     image: img5,
     link1: "https://github.com/NafilaNafi/phone-verification",
@@ -66,7 +56,7 @@ const portfolios = [
     text: "phone Numbers Verification App",
   },
   {
-    id: 7,
+    id: 6,
     category: "Javascript",
     image: img4,
     link1: "https://github.com/NafilaNafi/traveler-website",
@@ -75,7 +65,7 @@ const portfolios = [
     text: "Book your trip to the popular destination using traveller",
   },
   {
-    id: 8,
+    id: 7,
     category: "Javascript",
     image: css1,
     link1: "https://github.com/NafilaNafi/NewYork-Times-Api",
@@ -84,7 +74,7 @@ const portfolios = [
     text: "Newyork Times API will display all the Top Updated news using API Integration.",
   },
   {
-    id: 9,
+    id: 8,
     category: "Javascript",
     image: css3,
     link1: "https://github.com/NafilaNafi/uceazy",
@@ -93,7 +83,7 @@ const portfolios = [
     text: "Complete website for college admissions guidance using Javascript, Bootstrap, php",
   },
   {
-    id: 10,
+    id: 9,
     category: "Javascript",
     image: css2,
     link1: "https://github.com/NafilaNafi/Todo",
